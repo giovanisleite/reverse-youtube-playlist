1a4e2b125b55c15d6b8c5bf48a5f85ff058c4a25
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-03-30 18:56:55 -0300

    Fix some names

--- reverse_youtube_playlist.py
+++ reverse_youtube_playlist.py
@@ -5,34 +5,33 @@ from urllib import parse
 YOUTUBE_KEY = config('YOUTUBE_API_KEY')
 
 def reverse_youtube_playlist(playlist_url):
-    playlist_id = get_playlist_id(playlist_url)
-    video_ids = extract_video_ids_from_playlist(playlist_id)
-    return build_playlist_url(video_ids[::-1])
+    video_ids = get_video_ids_from_playlist(playlist_url)
+    video_ids_reversed = video_ids[::-1]
+    return build_playlist_url(video_ids_reversed)
 
 def build_playlist_url(video_ids):
     new_playlist_url = (
         f"https://www.youtube.com/watch_videos?video_ids={','.join(video_ids)}"
     )
-    print(new_playlist_url)
-
     return new_playlist_url
 
-def extract_video_ids_from_playlist(playlist_id):
-    playlist_items = get_plalist_information(playlist_id)
+def get_video_ids_from_playlist(playlist_url):
+    playlist_id = extract_playlist_id(playlist_url)
+    playlist_items = get_playlist_information(playlist_id)
 
     video_ids = []
     while(True):
-        video_ids = video_ids + extract_videos_ids_from_response(playlist_items)
+        video_ids = video_ids + extract_video_ids_from_response(playlist_items)
         next_page_token = playlist_items.get('nextPageToken', False)
         if not next_page_token:
             break
-        playlist_items = get_plalist_information(playlist_id, next_page_token)
+        playlist_items = get_playlist_information(playlist_id, next_page_token)
     return video_ids
 
-def get_playlist_id(playlist_url):
+def extract_playlist_id(playlist_url):
     return parse.parse_qs(parse.urlparse(playlist_url).query)['list'][0]
 
-def get_plalist_information(playlist_id, page_token=None):
+def get_playlist_information(playlist_id, page_token=None):
     page_token_parameter = f'?pageToken={page_token}' if page_token else ''
 
     api_url = (
@@ -43,7 +42,7 @@ def get_plalist_information(playlist_id, page_token=None):
 
     return requests.get(api_url).json()
 
-def extract_videos_ids_from_response(response):
+def extract_video_ids_from_response(response):
     return [
         video['snippet']['resourceId']['videoId']
         for video in response['items']
